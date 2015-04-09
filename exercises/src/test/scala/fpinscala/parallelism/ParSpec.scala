c26c40df189f140b06c7d3a7afd4b45b26010750
Jonas Fonseca <jonas.fonseca@gmail.com>
2015-04-09 12:22:07 -0400

    Par: exercise 7.3 - improved map2

---
+++ exercises/src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -0,0 +1,72 @@
+package fpinscala.parallelism
+
+import java.util.concurrent._
+import scala.concurrent.duration._
+import fpinscala.parallelism._
+
+import org.specs2.mutable.Specification
+import org.specs2.matcher.Matchers
+import org.specs2.specification.Scope
+
+class ParSpecification extends Specification with Matchers {
+
+  trait ThreadPoolContext extends Scope {
+    private val startTime = System.currentTimeMillis
+    private val asyncThreadCount = new atomic.AtomicInteger
+    def elapsedTime = System.currentTimeMillis millis
+    val pool: ExecutorService = Executors.newCachedThreadPool(new ThreadFactory {
+      override def newThread(r: Runnable) = {
+        asyncThreadCount.incrementAndGet
+        Executors.defaultThreadFactory.newThread(r)
+      }
+    })
+    def threadCount = asyncThreadCount.get
+  }
+
+  class StringParOps(val s: String) {
+    def after(delay: Duration) = {
+      Thread.sleep(delay toMillis)
+      Par.unit(s)
+    }
+  }
+  implicit def string2StringParOps(s: String) = new StringParOps(s)
+
+  def concat(a: String, b: String) = s"$a$b"
+
+  "Exercise 7.3".p
+
+  "Par.map2" should {
+    "wait for both computations when get is called" in new ThreadPoolContext {
+      val par = Par.map2(Par.fork("a1" after 500.millis), Par.fork("b1" after 1000.millis))(concat)
+
+      Par.run(pool)(par).get === "a1b1"
+      threadCount must be_>(1)
+      elapsedTime must be_>=(1000 millis)
+    }
+  
+    "not fail if computation takes longer than requested" in new ThreadPoolContext {
+      val par = Par.map2(Par.fork("a2" after 500.millis), Par.fork("b2" after 1000.millis))(concat)
+
+      Par.run(pool)(par).get(700, TimeUnit.MILLISECONDS) === "a2b2"
+      threadCount must be_>(1)
+      elapsedTime must be_>=(1000 millis)
+    }
+  }
+  
+  "Par.map2Timed" should {
+    "wait for both computations when get is called" in new ThreadPoolContext {
+      val par = Par.map2Timed(Par.fork("a3" after 500.millis), Par.fork("b3" after 1000.millis))(concat)
+
+      Par.run(pool)(par).get === "a3b3"
+      threadCount must be_>(1)
+      elapsedTime must be_>=(1000 millis)
+    }
+  
+    "fail if computation takes too long" in new ThreadPoolContext {
+      val par = Par.map2Timed(Par.fork("a4" after 500.millis), Par.fork("b4" after 1000.millis))(concat)
+      def exec = Par.run(pool)(par).get(700, TimeUnit.MILLISECONDS)
+
+      exec must throwA[TimeoutException]
+    }
+  }
+}
