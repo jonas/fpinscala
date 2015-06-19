86537c3604bedc759b1c9ef5e97bcbaee2bfb261
Jonas Fonseca <jonas.fonseca@mediative.com>
2015-06-18 21:25:34 -0400

    Exercises up until reader monad

--- exercises/src/test/scala/fpinscala/monads/MonadSpec.scala
+++ exercises/src/test/scala/fpinscala/monads/MonadSpec.scala
@@ -23,40 +23,42 @@ class MonadSpec extends Specification with Matchers {
   "Exercise 11.1" p
 
   implicit object GenMonadScope extends IntMonadScopeWithResult[Gen, Option] {
-    def create(i: Int) = Gen.choose(i, i + 100)
-    def run(ma: Gen[Int]) = Some(ma.sample.run(RNG.Simple(42))._1)
-    def runList(ma: Gen[List[Int]]) = Some(ma.sample.run(RNG.Simple(42))._1)
+    def genFrom = i => Gen.choose(i, i + 100)
+    def run[B] = ma => Some(ma.sample.run(RNG.Simple(42))._1)
   }
 
   implicit object ParMonadScope extends IntMonadScopeWithResult[Par, Option] with ThreadPoolContext {
-    def create(i: Int) = Par.lazyUnit(i)
-    def run(ma: Par[Int]) = Some(Par.run(pool)(ma).get)
-    def runList(ma: Par[List[Int]]) = Some(Par.run(pool)(ma).get)
+    def genFrom = i => Par.lazyUnit(i)
+    def run[B] = ma => Some(Par.run(pool)(ma).get)
   }
 
   type ErrorOr[A] = Either[ParseError, A]
 
   implicit object ParserMonadScope extends MonadScope[String, Parser, ErrorOr] {
     val gen = Gen.string('a', 'a')
-    def create(s: String) = KParsers.string(s)
-    def run(ma: Parser[String]) = KParsers.run(ma)("aaaaaaaaaaa")
-    def runList(ma: Parser[List[String]]) = KParsers.run(ma)("aaaaaaaa")
+    def genFrom = s => KParsers.string(s)
+    def run[B] = ma => KParsers.run(ma)("aaaaaaaaaaaaaaaaaaaaaaaa")
+  }
+
+  implicit object StateMonadScope extends IntMonadScopeWithResult[RNGState, Option] {
+    def genFrom = i => State.unit(i)
+    def run[B] = ma => Some(ma.run(RNG.Simple(42))._1)
   }
 
   implicit object OptionMonadScope extends IntMonadScope[Option] {
-    def create(i: Int) = if (i < 0) None else Some(i)
+    def genFrom = i => if (i < 0) None else Some(i)
   }
 
   implicit object StreamMonadScope extends IntMonadScope[Stream] {
-    def create(i: Int) = Stream.from(i) take 50
+    def genFrom = i => Stream.from(i) take 50
   }
 
   implicit object ListMonadScope extends IntMonadScope[List] {
-    def create(i: Int) = if (i < 0) Nil else (0 to i) toList
+    def genFrom = i => if (i < 0) Nil else (0 to i) toList
   }
 
   implicit object IdMonadScope extends IntMonadScope[Id] {
-    def create(i: Int) = Id(i)
+    def genFrom = i => Id(i)
   }
 
   MonadSpecs("genMonad", genMonad)
@@ -66,23 +68,22 @@ class MonadSpec extends Specification with Matchers {
   MonadSpecs("streamMonad", streamMonad)
   MonadSpecs("listMonad", listMonad)
   MonadSpecs("idMonad", idMonad)
+  MonadSpecs("stateMonad", stateMonad)
 
   /* ========================== */
 
-  trait MonadScope[A, M[A], R[A]] {
-    def create(a: A): M[A]
-    def run(ma: M[A]): R[A]
-    def runList(ma: M[List[A]]): R[List[A]]
+  trait MonadScope[A, M[_], R[_]] {
+    def run[B]: M[B] => R[B]
     def gen: Gen[A]
+    def genFrom: A => M[A]
+  }
+
+  trait IntMonadScope[M[_]] extends IntMonadScopeWithResult[M, M] {
+    def run[B] = identity
   }
 
   trait IntMonadScopeWithResult[M[Int], R[Int]] extends MonadScope[Int, M, R] {
     val gen = Gen.choose(-100, 100)
-  }
-
-  trait IntMonadScope[M[Int]] extends IntMonadScopeWithResult[M, M] {
-    def run(ma: M[Int]) = ma
-    def runList(ma: M[List[Int]]) = ma
   }
 
   def MonadSpecs[A, M[A], R[A]](name: String, monad: Monad[M])(implicit scope: MonadScope[A, M, R]): Unit = {
@@ -97,7 +98,80 @@ class MonadSpec extends Specification with Matchers {
           for { n <- Gen.choose(0, 10); l <- Gen.listOfN(n, scope.gen) } yield l
         } {
           case l =>
-	    scope.runList(monad.sequence(l.map(monad.unit(_)))) === scope.runList(monad.unit(l))
+	    scope.run(monad.sequence(l.map(monad.unit(_)))) === scope.run(monad.unit(l))
+        } must pass
+      }
+
+      "implement traverse" in {
+        Prop.forAll {
+          for { n <- Gen.choose(0, 10); l <- Gen.listOfN(n, scope.gen) } yield l
+        } {
+          case l =>
+            scope.run(monad.traverse(l)(monad.unit(_))) === scope.run(monad.unit(l))
+        } must pass
+      }
+
+      "implement replicateM" in {
+        Prop.forAll {
+          for { n <- Gen.choose(0, 10); a <- scope.gen } yield (n, a)
+        } {
+          case (n, a) =>
+            val l: List[A] = (for (i <- 0 until n) yield a) toList
+
+            scope.run(monad.replicateM(n, monad.unit(a))) === scope.run(monad.unit(l))
+        } must pass
+      }
+
+      "implement filterM" in {
+        val filterHead =
+          Prop.forAll {
+            for { n <- Gen.choose(0, 10); l <- Gen.listOfN(n, scope.gen) } yield l
+          } {
+            case l =>
+              def noHead(a: A) = Some(a) == l.headOption
+
+              scope.run(monad.filterM(l)(a => monad.unit(noHead(a)))) ===
+                scope.run(monad.unit(l.filter(noHead)))
+          }
+
+        val filterAll =
+          Prop.forAll {
+            for { n <- Gen.choose(0, 10); l <- Gen.listOfN(n, scope.gen) } yield l
+          } {
+            case l =>
+              scope.run(monad.filterM(l)(_ => monad.unit(true))) === scope.run(monad.unit(l)) &&
+              scope.run(monad.filterM(l)(_ => monad.unit(false))) === scope.run(monad.unit(Nil))
+          }
+
+        (filterHead && filterAll) must pass
+      }
+
+      "implement compose" in {
+        Prop.forAll {
+          for { a <- scope.gen } yield a
+        } {
+          case a =>
+            scope.run(monad.compose(
+              (a: A)      => monad.unit(a.toString),
+              (b: String) => monad.unit(b.length))(a)) === scope.run(monad.unit(a.toString.length))
+        } must pass
+      }
+
+      "implement flatMap in terms of compose" in {
+        Prop.forAll {
+          for { a <- scope.gen } yield monad.unit(a)
+        } {
+          case ma =>
+            scope.run(monad.flatMap(ma)(x => monad.unit(x))) === scope.run(ma)
+        } must pass
+      }
+
+      "implement join in terms of flatMap" in {
+        Prop.forAll {
+          for { a <- scope.gen } yield monad.unit(a)
+        } {
+          case ma =>
+            scope.run(monad.join(monad.unit(ma))) === scope.run(ma)
         } must pass
       }
     }
@@ -108,7 +182,7 @@ class MonadSpec extends Specification with Matchers {
     def fnGen =
       for (a <- ga)
         yield new Function1[A, M[A]] {
-          def apply(a: A) = scope.create(a)
+          def apply(a: A) = scope.genFrom(a)
           override def toString = s"i => Gen.choose($a, i)"
         }
 
@@ -133,7 +207,6 @@ class MonadSpec extends Specification with Matchers {
 
     associativeLaw && identityLaw
   }
-
 
   type MonadLaw = Prop
 
